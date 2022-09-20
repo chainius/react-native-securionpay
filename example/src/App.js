f36c4782470335a48c59f021dfc526684af323b4
Abdelilah El mansouri <contact@techadvisor.be>
2022-09-20 10:32:25 +0200

    fix dependecies

---
+++ example/src/App.js
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import { StyleSheet, View, Text, Button } from 'react-native';
+import { Init, LaunchPayment } from 'react-native-securionpay-sdk';
+Init('<api key>', '<bundleidentifier for ios | signature >');
+export default function App() {
+  const [result, setResult] = React.useState();
+  var launch = function () {
+    // test data from https://securionpay.com/docs/testing
+    LaunchPayment(
+      '4012001800000016', // cardnumber
+      '12', // exp month
+      '2022', // exp year
+      '123', // cvv
+      1000, // amount multuplied by 100
+      'EUR' // currency
+    )
+      .then((x) => {
+        setResult(JSON.stringify(x));
+        console.log(x);
+      })
+      .catch((x) => {
+        setResult(JSON.stringify(x.message));
+        console.log(x);
+      });
+  };
+  return React.createElement(
+    View,
+    { style: styles.container },
+    React.createElement(Text, null, result),
+    React.createElement(Button, { title: 'Pay', onPress: launch }, 'Pay')
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  box: {
+    width: 60,
+    height: 60,
+    marginVertical: 20,
+  },
+});
---
+++ lib/src/index.d.ts
@@ -0,0 +1,3 @@
+export declare function Init(publickey: String, signature?: String): Promise<any>;
+export declare function LaunchPayment(cardnumber: String, expirymonth: String, expiryyear: String, cvv: String, amount: Number, currency: String): Promise<any>;
+//# sourceMappingURL=index.d.ts.map
